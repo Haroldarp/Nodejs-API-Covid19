4a5f0ba21532082dede25788ee425254c771f81a
harold rodriguez <haroldarp12@gmail.com>
2020-07-30 20:10:05 -0400

    Antes de subir a heroku

--- controller/controller.js
+++ controller/controller.js
@@ -4,7 +4,7 @@ var controller = {
 
     addCase: async function(req, res){
 
-        var data = {
+        var persona = {
             Nombre : req.body.Nombre,
             Sexo: req.body.Sexo,
             Provincia: req.body.Provincia,
@@ -28,6 +28,10 @@ var controller = {
             }
         };
 
+        var day = `${persona.FechaReporte.getDate()}-${(persona.FechaReporte.getMonth() +1)}-${persona.FechaReporte.getFullYear()}`;
+        var month = `${(persona.FechaReporte.getMonth() +1)}-${persona.FechaReporte.getFullYear()}`;
+        var year = `${persona.FechaReporte.getFullYear()}`;
+
         try {
             const batch = firebase.db.batch();
             const increment = firebase.firestore.FieldValue.increment(1);
@@ -36,13 +40,59 @@ var controller = {
             var provinciaRef = await firebase.db.collection('Estadisticas').doc('Provincias');
             var municipioRef = await firebase.db.collection('Estadisticas').doc('Municipios');
             var sexoRef = await firebase.db.collection('Estadisticas').doc('Sexo');
+            var edadRef = await firebase.db.collection('Estadisticas').doc('Edad');
             var totalRef = await firebase.db.collection('Estadisticas').doc('Total');
+            var dayRef = await firebase.db.collection('Estadisticas').doc(day);
+            var monthRef = await firebase.db.collection('Estadisticas').doc(month);
+            var yearRef = await firebase.db.collection('Estadisticas').doc(year);
 
-            batch.set(personaRef , data);
-            batch.set(provinciaRef , {[data.Provincia]: increment}, {merge: true});
-            batch.set(municipioRef , {[data.Municipio]: increment}, {merge: true});
-            batch.set(sexoRef , {[data.Sexo]: increment}, {merge: true});
+            var doc = await dayRef.get();
+            if(!doc.exists){
+                batch.set(dayRef , {Femenino: 0, Masculino: 0}, {merge: true});
+            }
+
+            doc = await monthRef.get();
+            if(!doc.exists){
+                batch.set(monthRef , {Femenino: 0, Masculino: 0}, {merge: true});
+            }
+
+            doc= await yearRef.get();
+            if(!doc.exists){
+                batch.set(yearRef , {Femenino: 0, Masculino: 0}, {merge: true});
+            }
+
+            var edad ="";
+
+            if(persona.Edad <= 12 ){
+                edad ="0-12";
+
+            }else if(persona.Edad <= 17){
+                edad ="13-17";
+
+            }else if(persona.Edad <= 30){
+                edad ="18-30";
+
+            }else if(persona.Edad <= 45){
+                edad ="31-45";
+
+            }else if(persona.Edad <= 60){
+                edad ="46-60";
+
+            }else{
+                edad ="+60";
+            }
+
+
+            batch.set(personaRef , persona);
+            batch.set(provinciaRef , {[persona.Provincia]: increment}, {merge: true});
+            batch.set(municipioRef , {[persona.Municipio]: increment}, {merge: true});
+            batch.set(sexoRef , {[persona.Sexo]: increment}, {merge: true});
+            batch.set(edadRef , {[edad]: increment}, {merge: true});
             batch.set(totalRef , {Counter: increment}, {merge: true});
+            batch.set(dayRef , {[persona.Sexo]: increment}, {merge: true});
+            batch.set(monthRef , {[persona.Sexo]: increment}, {merge: true});
+            batch.set(yearRef , {[persona.Sexo]: increment}, {merge: true});
+
             await batch.commit();
 
             return res.status(200).send({
@@ -60,9 +110,119 @@ var controller = {
 
     },
 
-    getStadistics: function(req, res){
+    getStadistics: async function(req, res){
 
-       
+        var provinciasTop = [{provincia: "", count:-1},{provincia: "", count:-1},
+        {provincia: "", count:-1},{provincia: "", count:-1},{provincia: "", count:-1}];
+
+        var municipiosTop = [{municipio: "", count:-1},{municipio: "", count:-1},
+        {municipio: "", count:-1},{municipio: "", count:-1},{municipio: "", count:-1}];
+
+        var sexoVsTiempo = [];
+
+        
+        try {
+            
+            var provincias = await firebase.db.collection('Estadisticas').doc('Provincias').get();
+            var municipios = await firebase.db.collection('Estadisticas').doc('Municipios').get()
+            var edades = await firebase.db.collection('Estadisticas').doc('Edad').get();
+            var sexo;
+
+            //sexo vs timpo
+            var date = new Date();
+            date.setDate(date.getDate() - 1);
+            var day = `${date.getDate()}-${(date.getMonth() +1)}-${date.getFullYear()}`;
+            counter = 0;
+            
+            sexo = await firebase.db.collection('Estadisticas').doc(day).get();
+
+            while(counter < 12 && sexo.exists){
+                sexoVsTiempo.push({date: day, Femenino: sexo.data().Femenino, Masculino: sexo.data().Masculino});
+                
+                date.setDate(date.getDate() - 1);
+                var day = `${date.getDate()}-${(date.getMonth() +1)}-${date.getFullYear()}`;
+                counter++;
+
+                sexo = await firebase.db.collection('Estadisticas').doc(day).get();
+
+            }
+    
+            //top provincias
+            for(const prop in provincias.data()){
+    
+                if(provincias.data()[prop] > provinciasTop[0]["count"]){
+                    provinciasTop[4] = provinciasTop[3];
+                    provinciasTop[3] = provinciasTop[2];
+                    provinciasTop[2] = provinciasTop[1];
+                    provinciasTop[1] = provinciasTop[0];
+                    provinciasTop[0] = {provincia: prop, count: provincias.data()[prop]};
+    
+                }else if(provincias.data()[prop] > provinciasTop[1]["count"]){
+                    provinciasTop[4] = provinciasTop[3];
+                    provinciasTop[3] = provinciasTop[2];
+                    provinciasTop[2] = provinciasTop[1];
+                    provinciasTop[1] = {provincia: prop, count: provincias.data()[prop]};
+    
+                }else if(provincias.data()[prop] > provinciasTop[2]["count"]){
+                    provinciasTop[4] = provinciasTop[3];
+                    provinciasTop[3] = provinciasTop[2];
+                    provinciasTop[2] = {provincia: prop, count: provincias.data()[prop]};
+    
+                }else if(provincias.data()[prop] > provinciasTop[3]["count"]){
+                    provinciasTop[4] = provinciasTop[3];
+                    provinciasTop[3] = {provincia: prop, count: provincias.data()[prop]};
+    
+                }else if(provincias.data()[prop] > provinciasTop[4]["count"]){
+                    provinciasTop[4] = {provincia: prop, count: provincias.data()[prop]};
+                }
+            }
+    
+            //top municipios
+            for(const prop in municipios.data()){
+    
+                if(municipios.data()[prop] > municipiosTop[0]["count"]){
+                    municipiosTop[4] = municipiosTop[3];
+                    municipiosTop[3] = municipiosTop[2];
+                    municipiosTop[2] = municipiosTop[1];
+                    municipiosTop[1] = municipiosTop[0];
+                    municipiosTop[0] = {municipio: prop, count: municipios.data()[prop]};
+    
+                }else if(municipios.data()[prop] > municipiosTop[1]["count"]){
+                    municipiosTop[4] = municipiosTop[3];
+                    municipiosTop[3] = municipiosTop[2];
+                    municipiosTop[2] = municipiosTop[1];
+                    municipiosTop[1] = {municipio: prop, count: municipios.data()[prop]};
+    
+                }else if(municipios.data()[prop] > municipiosTop[2]["count"]){
+                    municipiosTop[4] = municipiosTop[3];
+                    municipiosTop[3] = municipiosTop[2];
+                    municipiosTop[2] = {municipio: prop, count: municipios.data()[prop]};
+    
+                }else if(municipios.data()[prop] > municipiosTop[3]["count"]){
+                    municipiosTop[4] = municipiosTop[3];
+                    municipiosTop[3] = {municipio: prop, count: municipios.data()[prop]};
+    
+                }else if(municipios.data()[prop] > municipiosTop[4]["count"]){
+                    municipiosTop[4] = {municipio: prop, count: municipios.data()[prop]};
+                }
+            }
+    
+            return res.status(200).send({
+                ok: true,
+                provinciasTop: provinciasTop,
+                municipiosTop: municipiosTop,
+                edades: edades.data(),
+                sexoVsTiempo: sexoVsTiempo
+            });
+
+        } catch (error) {
+             return res.status(200).send({
+                        ok: false,
+                        mesage: error
+                    });
+        }
+
+
     },
 
     addAdmin: async function(req, res){
@@ -106,6 +266,67 @@ var controller = {
                     mesage: err
                 });
             });
+    },
+
+    getTableInfo: async function(req, res){
+        
+        var info = [];
+
+        try {
+            var snapshot = await firebase.db.collection('Persona').orderBy('FechaReporte', 'desc').limit(500).get();
+
+            snapshot.forEach(doc =>{
+                info.push({
+                    Cedula: doc.data().Cedula,
+                    Nombre:doc.data().Nombre,
+                    Edad: doc.data().Edad,
+                    FechaInicioSintoma: doc.data().FechaInicioSintoma.toDate(),
+                    FechaReporte: doc.data().FechaReporte.toDate()
+                });
+            })
+
+            return res.status(200).json({
+                ok: true,
+                info: info
+            });
+
+        } catch (error) {
+            return res.status(200).send({
+                ok:false, 
+                mesage: error
+            });
+        }
+        
+    },
+
+    filterTable:async function(req, res){
+        var info = [];
+
+        try {
+            var snapshot = await firebase.db.collection('Persona').orderBy("Cedula").limit(500)
+            .startAt(req.query.Cedula).endAt(req.query.Cedula+'\uf8ff').get();
+                
+            snapshot.forEach(doc =>{
+                info.push({
+                    Cedula: doc.data().Cedula,
+                    Nombre:doc.data().Nombre,
+                    Edad: doc.data().Edad,
+                    FechaInicioSintoma: doc.data().FechaInicioSintoma.toDate(),
+                    FechaReporte: doc.data().FechaReporte.toDate()
+                });
+            })
+    
+            return res.status(200).json({
+                ok: true,
+                info: info
+            })
+            
+        } catch (error) {
+             return res.status(200).send({
+                ok:false, 
+                mesage: error
+            });
+        }
     },
 
     changeInfo: function(req, res){
--- routes/routes.js
+++ routes/routes.js
@@ -4,10 +4,12 @@ var projectController = require('../controller/controller');
 var router = express.Router();
 
 router.post('/addCase', projectController.addCase);
-// router.get('/getStadistics', projectController.getStadistics);
+router.get('/getStadistics', projectController.getStadistics);
 router.get('/getInfo', projectController.getInfo);
 router.post('/changeInfo', projectController.changeInfo);
 router.post('/addAdmin', projectController.addAdmin);
+router.get('/getTableInfo', projectController.getTableInfo);
+router.get('/filterTable', projectController.filterTable);
 
 
 module.exports = router;
