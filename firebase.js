40d414e22098013e28eddc5afcd1f061232c2b00
Harold Rodriguez <62029812+Haroldarp@users.noreply.github.com>
2021-01-26 23:45:45 -0400

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -9,15 +9,7 @@ module.exports.db = admin.firestore();
 
 const auth = require('firebase');
 
-var firebaseConfig = {
-  apiKey: "AIzaSyCQ97PI1np3Lvh-PboeR0ms55m8IVIzGlc",
-  authDomain: "lisa-vtohee.firebaseapp.com",
-  databaseURL: "https://lisa-vtohee.firebaseio.com",
-  projectId: "lisa-vtohee",
-  storageBucket: "lisa-vtohee.appspot.com",
-  messagingSenderId: "87650061864",
-  appId: "1:87650061864:web:458887505ff143d0f82f1d"
-};
+var firebaseConfig = "API_KEY_JSON_HERE";
 
 auth.initializeApp(firebaseConfig);
 
